cd775642a29d08519428a066d0764b406e8aba96
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-04 21:58:48 -0300

    update

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/catch'
 import { Restaurant } from './restaurant/restaurant.model';
 import { MEAT_API } from '../app.api';
 import { ErrorHandler } from 'app/app.error-handler';
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -30,4 +31,11 @@ export class RestaurantsService {
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
     }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    }
+
 }
